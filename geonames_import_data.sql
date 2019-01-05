ae06bd16769f7b21df6cff72f89a8c96a11edc6c
AGPDev <anderson@oceancrm.com.br>
2019-01-05 18:32:57 -0200

    Fix vaccum command

--- geonames_import_data.sql
+++ geonames_import_data.sql
@@ -1,43 +1,43 @@
 \COPY geoname (geonameid, name, asciiname, alternatenames, latitude, longitude, fclass, fcode, country, cc2, admin1, admin2, admin3, admin4, population, elevation, gtopo30, timezone, moddate) from 'data/allCountries.txt' null as '';
 ALTER TABLE ONLY geoname
 ADD CONSTRAINT pk_geonameid PRIMARY KEY (geonameid);
-vacuum analyze verbose geoname;
+VACUUM FULL geoname;
 
 \COPY alternatename (alternatenameid, geonameid, isoLanguage, alternateName, isPreferredName, isShortName, isColloquial, isHistoric) from 'data/alternateNames.txt' null as '';
 ALTER TABLE ONLY alternatename
 ADD CONSTRAINT pk_alternatenameid PRIMARY KEY (alternatenameid);
 ALTER TABLE ONLY alternatename
 ADD CONSTRAINT fk_geonameid FOREIGN KEY (geonameid) REFERENCES geoname(geonameid);
-vacuum analyze verbose alternatename;
+VACUUM FULL alternatename;
 
 \COPY iso_languagecodes (iso_639_3, iso_639_2, iso_639_1, language_name) from 'data/iso-languagecodes.txt' null as '';
-vacuum analyze verbose iso_languagecodes;
+VACUUM FULL iso_languagecodes;
 
 \COPY admin1CodesAscii (code, name, nameAscii, geonameid) from 'data/admin1CodesASCII.txt' null as '';
-vacuum analyze verbose admin1CodesAscii;
+VACUUM FULL admin1CodesAscii;
 
 \COPY admin2Codes (code, name, nameAscii, geonameid) from 'data/admin2Codes.txt' null as '';
-vacuum analyze verbose admin2Codes;
+VACUUM FULL admin2Codes;
 
 \COPY hierarchy (parentId, childId, type) from 'data/hierarchy.txt' null as '';
-vacuum analyze verbose hierarchy;
+VACUUM FULL hierarchy;
 
 \COPY featureCodes (code, name, description) from 'data/featureCodes_en.txt' null as '';
-vacuum analyze verbose featureCodes;
+VACUUM FULL featureCodes;
 
 \COPY timeZones (countrycode, timeZoneId, GMT_offset, DST_offset, raw_offset) from 'data/timeZones.txt' null as '';
-vacuum analyze verbose timeZones;
+VACUUM FULL timeZones;
 
 \COPY countryinfo (iso_alpha2, iso_alpha3, iso_numeric, fips_code, name, capital, areaInSqKm, population, continent, tld, currency, currencyName, phone, postalCodeFormat, postalCodeRegex, languages, geonameid, neighbours, equivalentFipsCode) from 'data/countryInfo.txt' null as '';
 ALTER TABLE ONLY countryinfo
 ADD CONSTRAINT fk_geonameid FOREIGN KEY (geonameid) REFERENCES geoname(geonameid);
-vacuum analyze verbose countryinfo;
+VACUUM FULL countryinfo;
 
 \COPY continentcodes (code, name, geonameId) from 'continentCodes.txt' null as '' delimiter ',';
-vacuum analyze verbose continentcodes;
+VACUUM FULL continentcodes;
 
 \COPY postalcodes (country, postal_code, name, admin1_name, admin1_code, admin2_name, admin2_code, admin3_name, admin3_code, latitude, longitude, accuracy) from 'data/zip_codes/allCountries.txt' null as '';
-vacuum analyze verbose postalcodes;
+VACUUM FULL postalcodes;
 
 CREATE INDEX index_timezones_all ON timezones (timezoneid, dst_offset, gmt_offset, raw_offset);
 CREATE INDEX index_continents_all ON continentcodes (geonameid, name, code);
